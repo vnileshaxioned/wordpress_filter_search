a0a97f74deeea3a59aa7a9155e8f0ba0527d5536
Nilesh Vishwakarma <nileshv@axioned.com>
2022-07-25 14:05:35 +0530

    added custom cpt and taxonomy in functions file

--- wp-config.php
+++ wp-config.php
@@ -20,19 +20,19 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'filter' );
 
 /** Database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'phpmyadmin' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', 'root' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
 
 /** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
+define( 'DB_CHARSET', 'utf8mb4' );
 
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
@@ -48,14 +48,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define( 'AUTH_KEY',         '<Ftx(V#f?tS0T;GELGEO70:[}6&+P^4TMKI+M SwrXj~-2h-GsS),#_vO5R-0cy#' );
+define( 'SECURE_AUTH_KEY',  '5Cg*hHULf8leTj#vNd=kdnx4|K.z$0dlyP?@LksjiT&|vy:VEf!qGYMa-?`2L5SZ' );
+define( 'LOGGED_IN_KEY',    'Y=/L>,t(%A-@~To$pe=qH/748>S8(azlQ$Jo0{;0:DE|Pn$rQ+PjN.V[-6O4bL0k' );
+define( 'NONCE_KEY',        '9r?P%#},GF6h.>-atN/JE9i{|l[>c/YX}dLj8wJttk9lZrm3MuSERb+eZ1zgJA>W' );
+define( 'AUTH_SALT',        'mU]@?qzi@3J7VtbTbx1yJ)GRVw`Z=~j*jV9;2)>9UwPGAxwV?c?j>(OgJjG&i8=`' );
+define( 'SECURE_AUTH_SALT', '4bZCa*t|:C]L`*~!J-dI2cjw8<jLX#n%4Vxk&Lzg5~(`K9sLe2([e I>a}5{YdzI' );
+define( 'LOGGED_IN_SALT',   'OL];kx !a$IrM&Y~Nj(mjXLFG=_%$1mfl{fi2^T,[*rp}Naj{cTd;qse8Qkf.#Ys' );
+define( 'NONCE_SALT',       ')&#(O-:_%ZKfY26/nx7qATQ}9/]kB5CQ cs:.pAoK2oy5U1H[w6Nx;.B9aFSXT,h' );
 
 /**#@-*/
 
---
+++ wp-content/themes/customtheme/functions.php
@@ -0,0 +1,83 @@
+<?php
+
+// enqueue the style and script files
+add_action('wp_enqueue_scripts', 'test_theme_script');
+function test_theme_script() {
+  wp_enqueue_style('custom-styling', get_stylesheet_uri());
+  wp_enqueue_style('custom-fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css');
+  wp_enqueue_script('custom-script', get_template_directory_uri().'/assets/js/script.js', array('jquery'), '', true);
+}
+
+// custom post type
+add_action( 'init', function() { custom_post_type('car', 'dashicons-car'); });
+function custom_post_type($cpt, $icon = '') {
+  $capitalize_cpt = ucfirst($cpt);
+  $labels = array(
+    'name'                => _x( $capitalize_cpt, 'Post Type General Name', 'customtheme' ),
+    'singular_name'       => _x( $cpt, 'Post Type Singular Name', 'customtheme' ),
+    'menu_name'           => __( $capitalize_cpt, 'customtheme' ),
+    'all_items'           => __( 'All '.$capitalize_cpt, 'customtheme' ),
+    'add_new'             => __( 'Add New', 'customtheme' ),
+    'edit_item'           => __( 'Edit '.$capitalize_cpt, 'customtheme' ),
+    'update_item'         => __( 'Update '.$capitalize_cpt, 'customtheme' ),
+    'search_items'        => __( 'Search '.$capitalize_cpt, 'customtheme' ),
+    'not_found'           => __( 'Not Found', 'customtheme' ),
+    'not_found_in_trash'  => __( 'Not found in Trash', 'customtheme' ),
+  );
+  $args = array(
+    'label'               => __( $cpt, 'customtheme' ),
+    'description'         => __( $cpt.' description', 'customtheme' ),
+    'labels'              => $labels,
+    'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail' ),
+    'hierarchical'        => true,
+    'public'              => true,
+    'show_ui'             => true,
+    'show_in_menu'        => true,
+    'show_in_nav_menus'   => true,
+    'show_in_admin_bar'   => true,
+    'menu_position'       => 4,
+    'menu_icon'           => $icon,
+    'can_export'          => true,
+    'has_archive'         => true,
+    'exclude_from_search' => true,
+    'publicly_queryable'  => true,
+    'capability_type'     => 'page',
+  );
+  register_post_type( $cpt, $args );
+}
+
+// custom taxonomy
+add_action( 'init', function() {
+  custom_taxonomy('brand', 'car');
+  custom_taxonomy('tag', 'car');
+});
+function custom_taxonomy($tax_name, $cpt) {
+  $capitalize_tax_name = ucfirst($tax_name);
+  $labels = array(
+    'name' => _x( $capitalize_tax_name, 'taxonomy general name' ),
+    'singular_name' => _x( $capitalize_tax_name, 'taxonomy singular name' ),
+    'search_items' =>  __( 'Search '.$capitalize_tax_name ),
+    'all_items' => __( 'All '.$capitalize_tax_name ),
+    'parent_item' => __( 'Parent '.$capitalize_tax_name ),
+    'parent_item_colon' => __( 'Parent '.$capitalize_tax_name ),
+    'edit_item' => __( 'Edit '.$capitalize_tax_name ),
+    'update_item' => __( 'Update '.$capitalize_tax_name ),
+    'add_new_item' => __( 'Add New '.$capitalize_tax_name ),
+    'new_item_name' => __( 'New '.$capitalize_tax_name ),
+    'menu_name' => __( $capitalize_tax_name ),
+  );
+  register_taxonomy( $tax_name, array($cpt), array(
+    'hierarchical' => true,
+    'labels' => $labels,
+    'show_ui' => true,
+    'show_in_rest' => true,
+    'show_admin_column' => true,
+    'query_var' => true,
+    'rewrite' => array( 'slug' => $tax_name ),
+  ));
+}
+
+// for classic editor
+add_filter('use_block_editor_for_post', '__return_false');
+
+?>
