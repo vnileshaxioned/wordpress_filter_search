9f03ec569e7569efe7cdef54d413515a59d8fdbf
Nilesh Vishwakarma <nileshv@axioned.com>
2022-07-25 17:02:30 +0530

    added menu class and container to nav menu in header file

--- wp-content/themes/customtheme/header.php
+++ wp-content/themes/customtheme/header.php
@@ -14,7 +14,10 @@
       <div class="wrapper">
         <h1><a href="<?php bloginfo('url') ?>" class="header-logo" title="<?php bloginfo('name') ?>"><?php bloginfo('name') ?></a></h1>
         <?php wp_nav_menu( array(
-          'theme_location' => 'primary'
+          'theme_location' => 'primary',
+          'menu_class' => 'navbar',
+          'container' => 'nav',
+          'items_wrap' => '<ul class="%2$s">%3$s</ul>',
         ) ); ?>
       </div>
     </header>
