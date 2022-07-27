cd0b0b362fdc7502f0c686f35e3bad7fe208d106
Nilesh Vishwakarma <nileshv@axioned.com>
2022-07-27 16:10:06 +0530

    added display filter template part module

--- wp-content/themes/customtheme/header.php
+++ wp-content/themes/customtheme/header.php
@@ -13,12 +13,18 @@
     <header>
       <div class="wrapper">
         <h1><a href="<?php bloginfo('url') ?>" class="header-logo" title="<?php bloginfo('name') ?>"><?php bloginfo('name') ?></a></h1>
-        <?php wp_nav_menu( array(
-          'theme_location' => 'primary',
-          'menu_class' => 'navbar',
-          'container' => 'nav',
-          'items_wrap' => '<ul class="%2$s">%3$s</ul>',
-        ) ); ?>
+        <?php
+          if (function_exists('custom_menu')) { 
+            custom_menu('primary');
+          } else {
+            wp_nav_menu( array(
+              'theme_location' => 'primary',
+              'menu_class' => 'menu',
+              'container' => 'nav',
+              'container_class' => 'navbar',
+              'items_wrap' => '<ul class="%2$s">%3$s</ul>',
+            ) );
+          } ?>
       </div>
     </header>
     <main>
