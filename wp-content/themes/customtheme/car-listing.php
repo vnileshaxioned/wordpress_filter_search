1dc17147e72d696739e53422a1e29f259ed01cee
Nilesh Vishwakarma <nileshv@axioned.com>
2022-07-28 10:42:19 +0530

    fix structure changes done inn content-display-filter file

--- wp-content/themes/customtheme/car-listing.php
+++ wp-content/themes/customtheme/car-listing.php
@@ -18,43 +18,21 @@
   <section class="posts">
     <div class="wrapper">
       <?php get_template_part('template-parts/modules/filter/filter'); ?>
-      <div class="post-container" data-posts="<?php echo $posts_per_page; ?>">
+      <ul class="post-container" data-posts="<?php echo $posts_per_page; ?>">
         <?php
-          while ($query -> have_posts()) {
-            $query -> the_post();
-            $id = get_the_ID();
-            $permalink = get_the_permalink();
-            $title = get_the_title();
-            $excerpt = get_the_excerpt(); 
-            $tags = get_the_terms($id, 'tag');
-            
-            if ($permalink || $title || $excerpt || $tags) { ?>
-            <article>
-              <?php
-                echo ($permalink && $title) ? '<h3><a href="'.$permalink.'" class="post-title" title="'.$title.'">'.$title.'</a></h3>' : null;
-                echo $excerpt ? '<p class="content-paragraph">'.$excerpt.'</p>' : null;
-                if ($tags) { ?>
-                <ul class="cpt_taxonomy">
-                  <?php
-                    foreach ($tags as $tag) {
-                      $tag_name = $tag->name;
-                      echo $tag_name ? '<li class="taxonomy-list">'.$tag_name.'</li>' : null;
-                    }
-                  ?>
-                </ul>
-              <?php } ?>
-            </article>
-          <?php }
-            } ?>
-        </div>
-      </div>
-    </section>
-  <?php } else { ?>
-    <section class="posts">
-      <div class="wrapper">
-        <p>No posts are available</p>
-      </div>
-    </section>
-  <?php }
-    wp_reset_postdata();
-    get_footer(); ?>
+          $args = array('query' => $query);
+          get_template_part('template-parts/modules/filter/content', 'display-filter', $args);
+        ?>
+      </ul>
+    </div>
+  </section>
+<?php } else { ?>
+  <section class="posts">
+    <div class="wrapper">
+      <p>No posts are available</p>
+    </div>
+  </section>
+<?php }
+  wp_reset_postdata();
+  get_footer();
+?>
--- wp-content/themes/customtheme/template-parts/modules/filter/content-display-filter.php
+++ wp-content/themes/customtheme/template-parts/modules/filter/content-display-filter.php
@@ -1,26 +1,32 @@
 <?php
-if ($query->have_posts()) {
-  while ($query->have_posts()) {
-  $query->the_post();
-  $id = get_the_ID();
-  $permalink = get_the_permalink();
-  $title = get_the_title();
-  $excerpt = get_the_excerpt(); 
-  $tags = get_the_terms($id, 'tag');
-  
-  if ($permalink || $title || $excerpt || $tags) {
-    $result = ($permalink && $title) ? '<article><h3><a href="'.$permalink.'" class="post-title" title="'.$title.'">'.$title.'</a></h3>' : null;
-    $result .= $excerpt ? '<p class="content-paragraph">'.$excerpt.'</p>' : null;
-    if ($tags) {
-    $result .= '<ul class="cpt_taxonomy">';
-      foreach ($tags as $tag) {
-        $tag_name = $tag->name;
-        $result .= $tag_name ? '<li class="taxonomy-list">'.$tag_name.'</li>' : null;
-      }
-    $result .= '</ul></article>';
+  $query = $args['query'];
+  while ($query -> have_posts()) {
+    $query -> the_post();
+    $id = get_the_ID();
+    $permalink = get_the_permalink();
+    $title = get_the_title();
+    $excerpt = get_the_excerpt(); 
+    $tags = get_the_terms($id, 'tag');
+    
+    if ($permalink || $title || $excerpt || $tags) { ?>
+    <li class="post-list">
+      <?php if ($permalink && $title) { ?>
+        <h3 class="post-heading">
+          <a href="<?php echo $permalink; ?>" class="post-title" title="<?php echo $title; ?>"><?php echo $title; ?></a>
+        </h3>
+      <?php }
+        echo $excerpt ? '<p class="content-paragraph">'.$excerpt.'</p>' : null;
+        if ($tags) { ?>
+        <ul class="taxonomy">
+          <?php
+            foreach ($tags as $tag) {
+              $tag_name = $tag->name;
+              echo $tag_name ? '<li class="taxonomy-list"><span>'.$tag_name.'</span></li>' : null;
+            }
+          ?>
+        </ul>
+      <?php } ?>
+    </li>
+  <?php }
     }
-    echo $result;
-  }
-  } wp_reset_postdata();
-}
-?>
+  ?>
