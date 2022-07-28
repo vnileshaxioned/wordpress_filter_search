961248dd8e9b4e03cb7e143b6bbde4e8a8955cef
Nilesh Vishwakarma <nileshv@axioned.com>
2022-07-28 19:25:04 +0530

    added sorting filter to taxonomy in script file

--- wp-content/themes/customtheme/assets/js/script.js
+++ wp-content/themes/customtheme/assets/js/script.js
@@ -1,87 +1,114 @@
 (function ($) {
+  $(document).ready(function () {
+    // taxonomy sorting
+    $('.sorting-tag').click(function () {
+      var taxonomy = $(this).attr('data-taxonomy');
+      var value = $('.sorting-tag option:selected').val();
+      sorting(value, taxonomy);
+    });
 
-  // for search
-  $('.search-input').keypress(function (e) {
-    if (e.key == 'Enter') {
+    $('.sorting-brand').click(function () {
+      var taxonomy = $(this).attr('data-taxonomy');
+      var value = $('.sorting-brand option:selected').val();
+      sorting(value, taxonomy);
+    });
+
+    function sorting(sort, taxonomy) {
+      var list = $('.taxonomy-column').find('.list-' + taxonomy);
+      $('.list-' + taxonomy).parent().append(
+        list.sort(function (a, b) {
+          if (sort == 'desc') {
+            return $(b).text() > $(a).text() ? 1 : -1;
+          } else {
+            return $(b).text() < $(a).text() ? 1 : -1;
+          }
+        })
+      );
+    }
+
+    // for search
+    $('.search-input').keypress(function (e) {
+      if (e.key == 'Enter') {
+        var data = $(this);
+        var inputValue = data.val();
+        searchFilter('', '', inputValue);
+      }
+    });
+
+    // for filter
+    var clickTagSlug = [];
+    var clickTagName = [];
+    $('.tax-name').click(function () {
       var data = $(this);
-      var inputValue = data.val();
-      searchFilter('', '', inputValue);
-    }
-  });
+      var tagName = data.attr('data-name');
+      var tagSlug = data.attr('data-tax');
 
-  // for filter
-  var clickTagSlug = [];
-  var clickTagName = [];
-  $('.tax-name').click(function () {
-    var data = $(this);
-    var tagName = data.attr('data-name');
-    var tagSlug = data.attr('data-tax');
-
-    if ($.inArray(tagName, clickTagName) == -1) {
-      clickTagName.push(tagName);
-    }
-    
-    if ($.inArray(tagSlug, clickTagSlug) != -1) {
-      clickTagSlug.splice($.inArray(tagSlug, clickTagSlug), 1);
-    } else {
-      clickTagSlug.push(tagSlug);
-    }
-    searchFilter(clickTagSlug, clickTagName, '');
-
-    // display filter
-    var list = [];
-    $.each(clickTagSlug, function (index, value) {
-      result = `<li class="selected-filter"><span>${value}</span></li>`;
-      list.push(result);
-    });
-    $('.display-filter').empty().append(list);
-  });
-
-  // remove selected filter
-  $(document).on('click', '.selected-filter', function () {
-    var data = $(this);
-    $('.tax-name').each(function () {
-      var taxonomy = $(this);
-      if (data.text() == taxonomy.attr('data-tax')) {
-        taxonomy.trigger('click');
+      if ($.inArray(tagName, clickTagName) == -1) {
+        clickTagName.push(tagName);
       }
-    });
-  });
 
-  // clear all filter
-  $('.clear-filter').click(function () {
-    $('.tax-name').each(function (index, value) {
-      var taxonomy = $(value);
-      if (taxonomy.is(':checked')) {
-        taxonomy.trigger('click');
+      if ($.inArray(tagSlug, clickTagSlug) != -1) {
+        clickTagSlug.splice($.inArray(tagSlug, clickTagSlug), 1);
+      } else {
+        clickTagSlug.push(tagSlug);
       }
-    });
-  });
+      searchFilter(clickTagSlug, clickTagName, '');
 
-  // searchFilter function to call ajax request
-  function searchFilter(tagSlug, tagName, search) {
-    var postPerPage = $('.post-container').attr('data-posts');
-    $.ajax({
-      type: 'post',
-      url: ajax.ajaxurl,
-      data: {
-        action: 'filter_search',
-        tag_name: tagName,
-        tag_slug: tagSlug,
-        search: search,
-        post_per_page: postPerPage,
-      },
-      datatype: 'json',
-      success: function (response) {
-        if (response.length) {
-          $('.post-container').html(response);
-        } else {
-          $('.post-container').html('<li><span>No Search found</span></li>');
+      // display filter
+      var list = [];
+      $.each(clickTagSlug, function (index, value) {
+        result = `<li class="selected-filter"><span>${value}</span></li>`;
+        list.push(result);
+      });
+      $('.display-filter').empty().append(list);
+    });
+
+    // remove selected filter
+    $(document).on('click', '.selected-filter', function () {
+      var data = $(this);
+      $('.tax-name').each(function () {
+        var taxonomy = $(this);
+        if (data.text() == taxonomy.attr('data-tax')) {
+          taxonomy.trigger('click');
         }
-      },
-      error: function (xhr, status, error) {
-        alert('Status: ' + xhr.status + ' ' + error);
-      },
+      });
     });
-  }
+
+    // clear all filter
+    $('.clear-filter').click(function () {
+      $('.tax-name').each(function (index, value) {
+        var taxonomy = $(value);
+        if (taxonomy.is(':checked')) {
+          taxonomy.trigger('click');
+        }
+      });
+    });
+
+    // searchFilter function to call ajax request
+    function searchFilter(tagSlug, tagName, search) {
+      var postPerPage = $('.post-container').attr('data-posts');
+      $.ajax({
+        type: 'post',
+        url: ajax.ajaxurl,
+        data: {
+          action: 'filter_search',
+          tag_name: tagName,
+          tag_slug: tagSlug,
+          search: search,
+          post_per_page: postPerPage,
+        },
+        datatype: 'json',
+        success: function (response) {
+          if (response.length) {
+            $('.post-container').html(response);
+          } else {
+            $('.post-container').html('<li><span>No Search found</span></li>');
+          }
+        },
+        error: function (xhr, status, error) {
+          alert('Status: ' + xhr.status + ' ' + error);
+        },
+      });
+    }
+  });
 }(jQuery));
