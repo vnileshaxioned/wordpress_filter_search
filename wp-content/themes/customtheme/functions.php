f021b96eda375e3ed808fcd4636cf4fecf8d19b9
Nilesh Vishwakarma <nileshv@axioned.com>
2022-07-28 10:30:21 +0530

    created content-display-filter for car-listing and ajax response

--- wp-content/themes/customtheme/functions.php
+++ wp-content/themes/customtheme/functions.php
@@ -75,35 +75,14 @@
         );
         array_push($tax_array, $output);
       }
-      
       $args['tax_query'] = $tax_array;
     }
     
     $query = new WP_Query($args);
 
     if ($query->have_posts()) {
-      while ($query->have_posts()) {
-        $query->the_post();
-        $id = get_the_ID();
-        $permalink = get_the_permalink();
-        $title = get_the_title();
-        $excerpt = get_the_excerpt(); 
-        $tags = get_the_terms($id, 'tag');
-        
-        if ($permalink || $title || $excerpt || $tags) {
-          $result = ($permalink && $title) ? '<article><h3><a href="'.$permalink.'" class="post-title" title="'.$title.'">'.$title.'</a></h3>' : null;
-          $result .= $excerpt ? '<p class="content-paragraph">'.$excerpt.'</p>' : null;
-          if ($tags) {
-            $result .= '<ul class="cpt_taxonomy">';
-                foreach ($tags as $tag) {
-                  $tag_name = $tag->name;
-                  $result .= $tag_name ? '<li class="taxonomy-list">'.$tag_name.'</li>' : null;
-                }
-            $result .= '</ul></article>';
-          }
-          echo $result;
-        }
-      } wp_reset_postdata();
+      $args = array('query' => $query);
+      get_template_part('template-parts/modules/filter/content', 'display-filter', $args);
     }
     die();
   }
--- wp-content/themes/customtheme/template-parts/modules/filter/content-display-filter.php
+++ wp-content/themes/customtheme/template-parts/modules/filter/content-display-filter.php
@@ -1,26 +1,29 @@
 <?php
-if ($query->have_posts()) {
-  while ($query->have_posts()) {
-  $query->the_post();
-  $id = get_the_ID();
-  $permalink = get_the_permalink();
-  $title = get_the_title();
-  $excerpt = get_the_excerpt(); 
-  $tags = get_the_terms($id, 'tag');
-  
-  if ($permalink || $title || $excerpt || $tags) {
-    $result = ($permalink && $title) ? '<article><h3><a href="'.$permalink.'" class="post-title" title="'.$title.'">'.$title.'</a></h3>' : null;
-    $result .= $excerpt ? '<p class="content-paragraph">'.$excerpt.'</p>' : null;
-    if ($tags) {
-    $result .= '<ul class="cpt_taxonomy">';
-      foreach ($tags as $tag) {
-        $tag_name = $tag->name;
-        $result .= $tag_name ? '<li class="taxonomy-list">'.$tag_name.'</li>' : null;
-      }
-    $result .= '</ul></article>';
+  $query = $args['query'];
+  while ($query -> have_posts()) {
+    $query -> the_post();
+    $id = get_the_ID();
+    $permalink = get_the_permalink();
+    $title = get_the_title();
+    $excerpt = get_the_excerpt(); 
+    $tags = get_the_terms($id, 'tag');
+    
+    if ($permalink || $title || $excerpt || $tags) { ?>
+    <article>
+      <?php
+        echo ($permalink && $title) ? '<h3><a href="'.$permalink.'" class="post-title" title="'.$title.'">'.$title.'</a></h3>' : null;
+        echo $excerpt ? '<p class="content-paragraph">'.$excerpt.'</p>' : null;
+        if ($tags) { ?>
+        <ul class="cpt_taxonomy">
+          <?php
+            foreach ($tags as $tag) {
+              $tag_name = $tag->name;
+              echo $tag_name ? '<li class="taxonomy-list">'.$tag_name.'</li>' : null;
+            }
+          ?>
+        </ul>
+      <?php } ?>
+    </article>
+  <?php }
     }
-    echo $result;
-  }
-  } wp_reset_postdata();
-}
 ?>
